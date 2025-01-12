14176e1859fb5ff8f474677279808578be09ea1a
KennedyKamau346 <59582496+KennedyKamau346@users.noreply.github.com>
2025-01-12 17:54:07 +0300

    Add files via upload

---
+++ p3_dotproduct.py
@@ -0,0 +1,10 @@
+import numpy as np
+# dot product with one neuron
+inputs = [1, 2, 3, 2.5]
+
+weights = [0.2, 0.8, -0.5, 1.0]
+
+bias = 2
+
+output = np.dot(weights, inputs) + bias
+print(output)
---
+++ p3_dotproduct_layer.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+inputs = [1, 2, 3, 2.5]      # vector
+
+weights = [[0.2, 0.8, -0.5, 1.0],          # matrix containing vectors
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]
+           ]
+
+biases = [2, 3, 0.5]
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+output = np.dot(weights, inputs) + biases
+print(output)
