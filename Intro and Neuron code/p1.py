ae64c779c245beab8b1acd1b52818bcc7a58edcb
KennedyKamau346 <59582496+KennedyKamau346@users.noreply.github.com>
2025-01-12 14:27:01 +0300

    Create p1.py

---
+++ Intro and Neuron code/p1.py
@@ -0,0 +1,9 @@
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3
+
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+
+# the above code is an example of a neuron. It's composed inputs, weights and bias and it 
+# multiplies input by weights then gets all their sum and then adds the bias to get the
+# final output
